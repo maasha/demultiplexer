852221145e22b88b1fafbaba8306698e4f49258d
Martin A. Hansen <mail@maasha.dk>
2015-03-22 21:41:08 +0100

    added empty test files

---
+++ test/helper.rb
@@ -0,0 +1,51 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+#                                                                              #
+# Copyright (C) 2014-2015 Martin Asser Hansen (mail@maasha.dk).                #
+#                                                                              #
+# This program is free software; you can redistribute it and/or                #
+# modify it under the terms of the GNU General Public License                  #
+# as published by the Free Software Foundation; either version 2               #
+# of the License, or (at your option) any later version.                       #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program; if not, write to the Free Software                  #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,    #
+# USA.                                                                         #
+#                                                                              #
+# http://www.gnu.org/copyleft/gpl.html                                         #
+#                                                                              #
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< #
+
+require 'demultiplexer'
+require 'test/unit'
+
+module Kernel
+  def capture_stdout
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out.string
+  ensure
+    $stdout = STDOUT
+  end
+
+  def capture_stderr
+    out = StringIO.new
+    $stderr = out
+    yield
+    return out.string
+  ensure
+    $stderr = STDERR
+  end
+end
+
+class Test::Unit::TestCase
+  def self.test(desc, &impl)
+    define_method("test #{desc}", &impl)
+  end
+end
---
+++ test/test_index_builder.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..')
+
+require 'test/helper'
+
+class TestIndexBuilder < Test::Unit::TestCase 
+end
